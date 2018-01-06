38575fc9c1a2276896910969dd68d9820bf8b73a
Paul Tan <pjxtan@gmail.com>
2018-01-06 14:30:32 -0500

    Refactoring and cleanup

--- src/kanban.js
+++ src/kanban.js
@@ -5,7 +5,7 @@ import {instanceOf} from 'prop-types';
 import {withCookies, Cookies} from 'react-cookie';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import fileDownload from 'js-file-download';
+// import fileDownload from 'js-file-download';
 import {LogActions, Logger} from './logger.js';
 
 import 'react-dragula/dist/dragula.min.css';
@@ -17,7 +17,6 @@ import './kanban.css';
 /*
 TODO:
 -- Figure out a good separation between board and logging logic
--- Delete task
 -- Implement command pattern to allow undo/redo, etc.
 -- Get the cookies actually working
 -- Import/export?
@@ -28,6 +27,7 @@ TODO:
 -- Cleanup package.json
 */
 
+// Drag-and-drop handler
 const drake = Dragula([], {
   isContainer: function (el) {
     return el.classList.contains('kanban-task-cards');
@@ -36,7 +36,7 @@ const drake = Dragula([], {
 
 /**** React Components ****/
 
-class KanbanBoard extends Component {
+class Board extends Component {
   static propTypes = {
     cookies: instanceOf(Cookies).isRequired
   };
@@ -55,61 +55,16 @@ class KanbanBoard extends Component {
     this.logItemsMaxChanged = this.logItemsMaxChanged.bind(this);
   }
 
+  /**** Task actions ****/
+
   addTask(column) {
-    const task = new KanbanTask('Do a thing', new Date(), ++this.taskCounter);
+    const task = new Task('Do a thing', new Date(), ++this.taskCounter);
     this.setState({
       tasks: this.state.tasks.concat([{'phase': column, 'task': task}])
     });
-    this.logAddTask(column, task);
-  }
-
-  // Wrapper for logging task additions
-  logAddTask(column, task) {
-    switch (column) {
-      case 'todo':
-        this.logAction(LogActions.ADDED_TASK_TO_TODO, {'task': task});
-        break;
-      case 'doing':
-        this.logAction(LogActions.ADDED_TASK_TO_DOING, {'task': task});
-        break;
-      case 'done':
-        this.logAction(LogActions.ADDED_TASK_TO_DONE, {'task': task});
-        break;
-      default:
-        break;
-    }
-  }
-
-  // Wrapper for logging task moves
-  logMoveTask(task, fromCol, toCol) {
-    switch (toCol) {
-      case 'todo':
-        this.logAction(LogActions.MOVED_TASK_TO_TODO, {'task': task, 'fromCol': fromCol});
-        break;
-      case 'doing':
-        this.logAction(LogActions.MOVED_TASK_TO_DOING, {'task': task, 'fromCol': fromCol});
-        break;
-      case 'done':
-        this.logAction(LogActions.MOVED_TASK_TO_DONE, {'task': task, 'fromCol': fromCol});
-        break;
-      default:
-        break;
-    }
-  }
-
-  // Wrapper for logging task title updates
-  logUpdateTaskTitle(task, oldTitle, newTitle) {
     this.logAction(
-      LogActions.UPDATED_TASK_TITLE,
-      {'task': task, 'oldTitle': oldTitle, 'newTitle': newTitle}
-    );
-  }
-
-  // Wrapper for logging task due date updates
-  logUpdateTaskDate(task, oldDate, newDate) {
-    this.logAction(
-      LogActions.UPDATED_TASK_DUE_DATE,
-      {'task': task, 'oldDate': oldDate, 'newDate': newDate}
+      LogActions.ADDED_TASK,
+      {'task': task, 'toCol': column}
     );
   }
 
@@ -119,35 +74,56 @@ class KanbanBoard extends Component {
     const newTasks = this.state.tasks.filter(el => el.task.id !== parseInt(taskId))
       .concat({phase: toPhase, task: task});
     this.setState({tasks: newTasks});
-    this.logMoveTask(task, fromPhase, toPhase);
+    this.logAction(
+      LogActions.MOVED_TASK,
+      {'task': task, 'toCol': toPhase, 'fromCol': fromPhase}
+    );
   }
 
-  // TODO: Refactor task updates (title, due date, etc.) to common method
+  deleteTask(taskId) {
+    const taskToDelete = this.state.tasks.find(el => el.task.id === parseInt(taskId)).task;
+    const newTasks = this.state.tasks.filter(el => el.task.id !== parseInt(taskId));
+    this.setState({tasks: newTasks});
+    this.logAction(
+      LogActions.DELETED_TASK,
+      {'task': taskToDelete}
+    );
+  }
+
+  // TODO: Refactor common task update logic
 
   updateTaskTitle(id, title) {
     const elemToReplace = this.state.tasks.find(el => el.task.id === parseInt(id));
     const oldTask = elemToReplace.task;
-    const updatedTask = new KanbanTask(title, oldTask.dueDate, oldTask.id);
+    const updatedTask = new Task(title, oldTask.dueDate, oldTask.id);
     const newTasks = this.state.tasks.filter(el => el.task.id !== parseInt(id))
       .concat({phase: elemToReplace.phase, task: updatedTask});
     this.setState({tasks: newTasks});
-    this.logUpdateTaskTitle(updatedTask, oldTask.title, updatedTask.title);
+    this.logAction(
+      LogActions.UPDATED_TASK_TITLE,
+      {'task': updatedTask, 'oldTitle': oldTask.title, 'newTitle': updatedTask.title}
+    );
   }
 
   updateTaskDate(id, date) {
     const elemToReplace = this.state.tasks.find(el => el.task.id === parseInt(id));
     const oldTask = elemToReplace.task;
-    const updatedTask = new KanbanTask(oldTask.title, moment(date), oldTask.id);
+    const updatedTask = new Task(oldTask.title, moment(date), oldTask.id);
     const newTasks = this.state.tasks.filter(el => el.task.id !== parseInt(id))
       .concat({phase: elemToReplace.phase, task: updatedTask});
     this.setState({tasks: newTasks});
-    this.logUpdateTaskDate(updatedTask, oldTask.dueDate, updatedTask.dueDate);
+    this.logAction(
+      LogActions.UPDATED_TASK_DATE,
+      {'task': updatedTask, 'oldDate': oldTask.dueDate, 'newDate': updatedTask.dueDate}
+    );
   }
 
   clearAllTasks() {
     this.setState({tasks: []});
     this.logAction(LogActions.CLEARED_ALL_TASKS);
   }
+
+  /**** Logbook actions ****/
 
   logItemsMaxChanged(props) {
     if (props.logItemsMax > 0) this.setState(props);
@@ -161,45 +137,52 @@ class KanbanBoard extends Component {
     });
   }
 
+  /**** Rendering ***/
+
+  // TODO: A lot of repeated code here...
+
   renderTodoColumn() {
     return (
-      <KanbanColumn title="To-do"
+      <Column title="To-do"
                     phase="todo"
                     tasks={this.state.tasks.filter(task => task.phase === 'todo').map(task => task.task)}
                     addTaskCallback={() => this.addTask('todo')}
                     titleChangedCallback={(id, title) => this.updateTaskTitle(id, title)}
                     dateChangedCallback={(id, date) => this.updateTaskDate(id, date)}
+                    deleteCallback={(id) => this.deleteTask(id)}
       />
     );
   }
 
   renderDoingColumn() {
     return (
-      <KanbanColumn title="Doing"
+      <Column title="Doing"
                     phase="doing"
                     tasks={this.state.tasks.filter(task => task.phase === 'doing').map(task => task.task)}
                     addTaskCallback={() => this.addTask('doing')}
                     titleChangedCallback={(id, title) => this.updateTaskTitle(id, title)}
                     dateChangedCallback={(id, date) => this.updateTaskDate(id, date)}
+                    deleteCallback={(id) => this.deleteTask(id)}
       />
     );
   }
 
   renderDoneColumn() {
     return (
-      <KanbanColumn title="Done"
+      <Column title="Done"
                     phase="done"
                     tasks={this.state.tasks.filter(task => task.phase === 'done').map(task => task.task)}
                     addTaskCallback={() => this.addTask('done')}
                     titleChangedCallback={(id, title) => this.updateTaskTitle(id, title)}
                     dateChangedCallback={(id, date) => this.updateTaskDate(id, date)}
+                    deleteCallback={(id) => this.deleteTask(id)}
       />
     );
   }
 
-  renderLog() {
+  renderLogBook() {
     return (
-      <KanbanLog items={this.state.logItems} maxItems={this.state.logItemsMax}/>
+      <LogBook items={this.state.logItems} maxItems={this.state.logItemsMax}/>
     )
   }
 
@@ -249,7 +232,7 @@ class KanbanBoard extends Component {
                   <a className="button is-outlined is-danger" onClick={this.clearAllTasks}>Clear all tasks</a>
                 </div>
               </div>
-              {this.renderLog()}
+              {this.renderLogBook()}
             </div>
             <div className="kanban-stats-container column">
               TBA
@@ -259,6 +242,8 @@ class KanbanBoard extends Component {
       </div>
     );
   }
+
+  /**** Lifecycle ****/
 
   componentDidMount() {
     // Handle the drop event: task is moved to a different column
@@ -281,7 +266,7 @@ class KanbanBoard extends Component {
   }
 }
 
-const KanbanColumn = function (props) {
+const Column = function (props) {
   return (
     <div className="kanban-column column">
       <h3 className="kanban-column-title title is-3">
@@ -293,10 +278,11 @@ const KanbanColumn = function (props) {
       <div className="kanban-task-cards" id={"kanban-task-cards-" + props.phase}>
         {props.tasks.map(task =>
           <nav className="level" id={"kanban-task-card-" + task.id} key={task}>
-            <KanbanTaskCard task={task}
+            <TaskCard task={task}
               // title={task.title} dueDate={task.dueDate}
                             titleChangedCallback={(title) => props.titleChangedCallback(task.id, title)}
                             dateChangedCallback={(date) => props.dateChangedCallback(task.id, date)}
+                            deleteCallback={() => props.deleteCallback(task.id)}
             />
           </nav>
         )}
@@ -305,39 +291,37 @@ const KanbanColumn = function (props) {
   );
 };
 
-class KanbanTaskCard extends Component {
+class TaskCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: props.task.title,
       dueDate: props.task.dueDate,
-      // TODO: Probably will just remove the description entirely
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       dateCreated: props.task.dateCreated
     };
     this.id = props.id;
-    // These binds ensure that "this" references the correct object (in this case, the KanbanTaskCard)
+    // These binds ensure that "this" references the correct object (in this case, the TaskCard)
     this.titleChanged = this.titleChanged.bind(this);
-    this.descriptionChanged = this.descriptionChanged.bind(this);
     this.dateChanged = this.dateChanged.bind(this);
     // Callbacks for updating task attributes
     this.titleChangedCallback = props.titleChangedCallback;
     this.dateChangedCallback = props.dateChangedCallback;
+    this.deleteCallback = props.deleteCallback;
   }
+
+  /**** Event handlers ****/
 
   titleChanged(props) {
     if (props.title !== "") this.setState(props);
     this.titleChangedCallback(props.title);
   }
 
-  descriptionChanged(props) {
-    if (props.description !== "") this.setState(props);
-  }
-
   dateChanged(date) {
     this.setState({dueDate: date});
     this.dateChangedCallback(date);
   }
+
+  /**** Rendering ****/
 
   render() {
     return (
@@ -369,7 +353,7 @@ class KanbanTaskCard extends Component {
                 </div>
               </div>
               <div className="level-right">
-                <a className="button is-small has-text-danger">
+                <a className="button is-small has-text-danger" onClick={this.deleteCallback}>
                   <span className="icon">
                   <i className="fa fa-trash"></i>
                   </span>
@@ -384,13 +368,13 @@ class KanbanTaskCard extends Component {
   }
 }
 
-const KanbanLog = function (props) {
+const LogBook = function (props) {
   return (
     <table className="kanban-log table is-narrow is-hoverable is-bordered">
       <thead>
       <tr>
         <th>Time</th>
-        <th>Event</th>
+        <th>Action</th>
       </tr>
       </thead>
       <tbody>
@@ -405,9 +389,13 @@ const KanbanLog = function (props) {
   );
 };
 
+/**** Command classes ****/
+
+// TODO
+
 /**** Data classes ****/
 
-class KanbanTask {
+class Task {
   constructor(title, dueDate, id) {
     this.title = title;
     this.dueDate = moment(dueDate);
@@ -416,7 +404,7 @@ class KanbanTask {
   }
 
   toString() {
-    return "[KanbanTask_" + this.id + "]";
+    return "[Task_" + this.id + "]";
   }
 }
 
@@ -424,4 +412,4 @@ class KanbanTask {
 
 const getIdFromDomId = (domId) => domId.split('-').slice(-1)[0];
 
-export default withCookies(KanbanBoard);
+export default withCookies(Board);
--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,11 @@
 
 export const LogActions = Object.freeze({
-  ADDED_TASK_TO_TODO: 0,
-  ADDED_TASK_TO_DOING: 1,
-  ADDED_TASK_TO_DONE: 2,
-  MOVED_TASK_TO_TODO: 3,
-  MOVED_TASK_TO_DOING: 4,
-  MOVED_TASK_TO_DONE: 5,
-  UPDATED_TASK_TITLE: 6,
-  CLEARED_ALL_TASKS: 7,
+  ADDED_TASK: 0,
+  MOVED_TASK: 1,
+  DELETED_TASK: 2,
+  UPDATED_TASK_TITLE: 3,
+  UPDATED_TASK_DATE: 4,
+  CLEARED_ALL_TASKS: 5,
 });
 
 const phases = {
@@ -16,64 +14,36 @@ const phases = {
   'done': 'Done'
 };
 
-// TODO: The logger still has a lot of dependencies on the Kanban board - how to better separate concerns?
-//       Maybe messages should still be constructed in the board class?
 export class Logger {
   static getLogItem(actionType, id, kwargs) {
-    let task;
-    let tasks;
-    let fromCol;
-    let fromColReadable;
-    if ('task' in kwargs) task = kwargs.task;
-    if ('tasks' in kwargs) tasks = kwargs.tasks;
-    if ('fromCol' in kwargs) {
-      fromCol = kwargs.fromCol;
-      fromColReadable = phases[fromCol];
-    }
     let message;
     switch (actionType) {
-      case LogActions.ADDED_TASK_TO_TODO:
-        message = 'Added task <b>' + task.title + '</b> to <b class="colour-text-todo">To-do</b>';
-        message += ' [' + (tasks.filter(el => el.phase === 'todo').length + 1) + ']';
-        message += ' (due: <b>' + task.dueDate.format("MMM DD") + '</b>)';
+      case LogActions.ADDED_TASK:
+        message = `Added <b>${kwargs.task.title}</b> `;
+        message += ` to <b class="colour-text-${kwargs.toCol}">${phases[kwargs.toCol]}</b>`;
+        message += ` (due: <b>${kwargs.task.dueDate.format("MMM DD")}</b>)`;
         break;
-      case LogActions.ADDED_TASK_TO_DOING:
-        message = 'Added task <b>' + task.title + '</b> to <b class="colour-text-doing">Doing</b>';
-        message += ' [' + (tasks.filter(el => el.phase === 'doing').length + 1) + ']';
-        message += ' (due: <b>' + task.dueDate.format("MMM DD") + '</b>)';
+      case LogActions.MOVED_TASK:
+        message = `Moved <b>${kwargs.task.title}</b>`;
+        message += ` from <b class="colour-text-${kwargs.fromCol}">${phases[kwargs.fromCol]}</b>`;
+        message += ` from <b class="colour-text-${kwargs.toCol}">${phases[kwargs.toCol]}</b>`;
         break;
-      case LogActions.ADDED_TASK_TO_DONE:
-        message = 'Added task <b>' + task.title + '</b> to <b class="colour-text-done">Done</b>';
-        message += ' [' + (tasks.filter(el => el.phase === 'done').length + 1) + ']';
-        message += ' (due: <b>' + task.dueDate.format("MMM DD") + '</b>)';
-        break;
-      case LogActions.MOVED_TASK_TO_TODO:
-        message = 'Moved task <b>' + task.title + '</b> from <b class="colour-text-' + fromCol + '">' + fromColReadable + '</b>';
-        message += ' to <b class="colour-text-todo">To-do</b>';
-        message += ' [' + (tasks.filter(el => el.phase === 'todo').length) + ']';
-        break;
-      case LogActions.MOVED_TASK_TO_DOING:
-        message = 'Moved task <b>' + task.title + '</b> from <b class="colour-text-' + fromCol + '">' + fromColReadable + '</b>';
-        message += ' to <b class="colour-text-doing">Doing</b>';
-        message += ' [' + (tasks.filter(el => el.phase === 'doing').length) + ']';
-        break;
-      case LogActions.MOVED_TASK_TO_DONE:
-        message = 'Moved task <b>' + task.title + '</b> from <b class="colour-text-' + fromCol + '">' + fromColReadable + '</b>';
-        message += ' to <b class="colour-text-done">Done</b>';
+      case LogActions.DELETED_TASK:
+        message = `Deleted <b>${kwargs.task.title}</b>`;
         break;
       case LogActions.UPDATED_TASK_TITLE:
-        message = 'Changed title of task <b>' + kwargs.oldTitle + '</b> to <b>' + kwargs.newTitle + '</b>';
+        message = `Changed title of <b>${kwargs.oldTitle}</b> to <b>${kwargs.newTitle}</b>`;
         break;
       case LogActions.UPDATED_TASK_DATE:
-        message = 'Changed due date of task <b>' + task.title + '</b>';
-        message += ' from <b>' + kwargs.oldDate.format('MMM DD, YYYY') + '</b>';
-        message += ' to <b>' + kwargs.newDate.format('MMM DD, YYYY') + '</b>';
+        message = `Changed due date of <b>${kwargs.task.title}</b>`;
+        message += ` from <b>${kwargs.oldDate.format('MMM DD')}</b>`;
+        message += ` to <b>${kwargs.newDate.format('MMM DD')}</b>`;
         break;
       case LogActions.CLEARED_ALL_TASKS:
         message = 'Cleared all tasks';
         break;
       default:
-        return;
+        throw "Invalid log action type";
     }
     return new LogItem(actionType, message, id)
   }
